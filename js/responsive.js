bf243e9bb3f094501892016ca5975b9e5d353797
Feireca <f.iregui310@uniandes.edu.co>
2017-08-29 16:44:19 -0500

    sobre mi

--- js/responsive.js
+++ js/responsive.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     var altura = $(window).height();
     $("#imagen").css({"height": altura});
     $("#home").css({"height": altura});
+    $("#trabajo").css({"height": altura});
+    $("#estudio").css({"height": altura});
+    $("#familia").css({"height": altura});
+    $("#yo").css({"height": altura});
 
     $("#iraopciones").click(function(){
         console.log("entro");
